6a9b1c812f1764bc451a7cf6cadaa0f255d26b3f
Erik <erikdesjardins@users.noreply.github.com>
2017-06-15 11:27:06 -0400

    use import-fresh to avoid rollup's global state instead of synchronizing access to it

--- index.js
+++ index.js
@@ -6,25 +6,10 @@
 'use strict';
 
 var path = require('path');
-var _rollup = require('rollup');
-
-// Rollup seems to have global state,
-// and so has strange behaviour when running multiple instances concurrently...
-var locked = false;
-var queue = [];
-
-function dequeue() {
-	if (queue.length) {
-		locked = true;
-		queue.shift()(_rollup).then(dequeue);
-	} else {
-		locked = false;
-	}
-}
-
-function withRollupInstance(callback) {
-	queue.push(callback);
-	if (!locked) dequeue();
+var importFresh = require('import-fresh');
+// Rollup seems to have global state, so get a fresh instance for every run...
+function getRollupInstance() {
+	return importFresh('rollup');
 }
 
 function splitRequest(request) {
@@ -51,56 +36,53 @@ module.exports = function(source, sourceMap) {
 
 	var entryId = this.resourcePath;
 
-	withRollupInstance(function(rollup) {
-		return rollup
-			.rollup({
-				entry: entryId,
-				external: external,
-				plugins: plugins.concat({
-					resolveId: function(id, importerId) {
-						if (id === entryId) {
-							return entryId;
-						} else {
-							return new Promise(function(resolve, reject) {
-								// split apart resource paths because Webpack's this.resolve() can't handle `loader!` prefixes
-								var parts = splitRequest(id);
-								var importerParts = splitRequest(importerId);
+	getRollupInstance().rollup({
+		entry: entryId,
+		external: external,
+		plugins: plugins.concat({
+			resolveId: function(id, importerId) {
+				if (id === entryId) {
+					return entryId;
+				} else {
+					return new Promise(function(resolve, reject) {
+						// split apart resource paths because Webpack's this.resolve() can't handle `loader!` prefixes
+						var parts = splitRequest(id);
+						var importerParts = splitRequest(importerId);
 
-								// resolve the full path of the imported file with Webpack's module loader
-								// this will figure out node_modules imports, Webpack aliases, etc.
-								this.resolve(path.dirname(importerParts.resource), parts.resource, function(err, fullPath) {
-									if (err) {
-										reject(err);
-									} else {
-										resolve(parts.loaders + fullPath);
-									}
-								});
-							}.bind(this));
+						// resolve the full path of the imported file with Webpack's module loader
+						// this will figure out node_modules imports, Webpack aliases, etc.
+						this.resolve(path.dirname(importerParts.resource), parts.resource, function(err, fullPath) {
+							if (err) {
+								reject(err);
+							} else {
+								resolve(parts.loaders + fullPath);
+							}
+						});
+					}.bind(this));
+				}
+			}.bind(this),
+			load: function(id) {
+				if (id === entryId) {
+					return { code: source, map: sourceMap };
+				}
+				return new Promise(function(resolve, reject) {
+					// load the module with Webpack
+					// this will apply all relevant loaders, etc.
+					this.loadModule(id, function(err, source, map, module) {
+						if (err) {
+							reject(err);
+							return;
 						}
-					}.bind(this),
-					load: function(id) {
-						if (id === entryId) {
-							return { code: source, map: sourceMap };
-						}
-						return new Promise(function(resolve, reject) {
-							// load the module with Webpack
-							// this will apply all relevant loaders, etc.
-							this.loadModule(id, function(err, source, map, module) {
-								if (err) {
-									reject(err);
-									return;
-								}
-								resolve({ code: source, map: map });
-							});
-						}.bind(this));
-					}.bind(this),
-				})
-			})
-			.then(function(bundle) {
-				var result = bundle.generate({ format: 'es', sourceMap: true });
-				callback(null, result.code, result.map);
-			}, function(err) {
-				callback(err);
-			});
-	}.bind(this));
+						resolve({ code: source, map: map });
+					});
+				}.bind(this));
+			}.bind(this),
+		})
+	})
+	.then(function(bundle) {
+		var result = bundle.generate({ format: 'es', sourceMap: true });
+		callback(null, result.code, result.map);
+	}, function(err) {
+		callback(err);
+	});
 };
